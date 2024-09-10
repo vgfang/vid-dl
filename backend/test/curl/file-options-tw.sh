f74d43ad337f0ed53a256e67c8af79b21319b3c6
vgfang <vgfang@yandex.com>
2024-09-10 01:01:00 -0700

    fixed service connection

---
+++ backend/test/curl/file-options-tw.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:3000/file-options \
+  -H "Content-Type: application/json" \
+  -d '{"url": "https://x.com/daily_glt/status/1775272407026311512}'
---
+++ backend/test/curl/file-options-yt.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:3000/file-options \
+  -H "Content-Type: application/json" \
+  -d '{"url": "https://www.youtube.com/watch?v=gXmznGEW9vo"}'
